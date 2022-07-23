0bf3b99ed4daf6fd0fe018d3afdf7975ba6d6210
hungvx <vuxuanhungmta@gmail.com>
2022-07-23 14:38:58 +0700

    define action get, post

---
+++ WebApplication1/WebApplication1/Pages/Movie/Edit2.cshtml.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Movie
+{
+    public class Edit2Model : PageModel
+    {
+        //public void OnGet()
+        //{
+        //}
+        private readonly WebApplication1.Data.WebApplication1Context _context;
+
+        public Edit2Model(WebApplication1.Data.WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGet3Async(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        [BindProperty]
+        public WebApplication1.Models.Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGet2Async(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost2Async()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(Movie.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.ID == id);
+        }
+    }
+}
