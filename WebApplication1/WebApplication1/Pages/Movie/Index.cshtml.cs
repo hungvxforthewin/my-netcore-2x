8b3f931f268535f219838be8afe3076e829a9064
hungvx <vuxuanhungmta@gmail.com>
2022-07-25 23:26:56 +0700

    add search string

--- WebApplication1/WebApplication1/Pages/Movie/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Movie/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Models;
@@ -13,6 +14,9 @@ namespace WebApplication1.Pages.Movie
     public class IndexModel : PageModel
     {
         private readonly WebApplication1.Data.WebApplication1Context _context;
+        private string searchString;
+        private SelectList genres;
+        private string movieGenre;
 
         public IndexModel(WebApplication1.Data.WebApplication1Context context)
         {
@@ -20,6 +24,25 @@ namespace WebApplication1.Pages.Movie
         }
 
         public IList<WebApplication1.Models.Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get => searchString; set => searchString = value; }
+        public SelectList Genres { get => genres; set => genres = value; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get => movieGenre; set => movieGenre = value; }
+
+
+        public async Task OnGetNewAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
+        }
+
 
         public async Task OnGetAsync()
         {
